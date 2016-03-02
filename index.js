41de047618adc999c52ad4080c7295c08dc9d495
Mejdi Ourir <m.ourir@ivsweb.com>
2016-03-02 15:27:15 +0100

    suppose that vlc is on the path in others platforms

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var path    = require('path');
 var cp      = require('child_process');
 
 
-var vlc_path = path.join(__dirname, "vlc/vlc.exe");
+var vlc_path ="vlc";
+
+if(process.platform == "win32")
+  vlc_path = path.join(__dirname, "vlc/vlc.exe");
 
 module.exports = function(/* args, options */){
 
