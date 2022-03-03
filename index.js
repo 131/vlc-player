0619c679bfc79a38354ba32d48530c057eca63dd
Francois Leurent <131.code@leurent.email>
2022-03-03 13:53:37 +0100

    Lookup installed VLC path

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict';
 
-const path    = require('path');
-const spawn   = require('child_process').spawn;
+const fs    = require('fs');
+const path  = require('path');
+const spawn = require('child_process').spawn;
 
-const vlc_path = path.join(__dirname, "vlc/vlc.exe");
+let vlc_path = [
+  path.join(process.env['ProgramFiles'], 'VideoLAN/VLC/vlc.exe'),
+  path.join(process.env['ProgramFiles(x86)'], 'VideoLAN/VLC/vlc.exe'),
+].find(bin => fs.existsSync(bin));
+
 
 module.exports = function(/* args, options */) {
+  if(!vlc_path)
+    throw `Cannot find vlc path`;
+
   return spawn.bind(null, vlc_path).apply(null, arguments);
-}
+};
+
